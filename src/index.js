c55645bae1406e39510438109b230d47d790da47
Oganesyan Artur <arturris@mail.ru>
2018-09-20 14:08:39 -0500

    new Version of the app

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import App from './App';
-import rootReducer from './store/reducers'
+import 'semantic-ui-css/semantic.min.css';
+import 'font-awesome/css/font-awesome.css'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-))
-
-
-const app = (
-    <Provider store={store}>
-        {/* <BrowserRouter> */}
-            <App />
-        {/* </BrowserRouter> */}
-    </Provider>
-)
-
-ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
